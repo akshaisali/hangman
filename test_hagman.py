c0a4d75f2724ed932d5d721fe3d13fbe59d52a19
Akshaisali <akshaisali007@gmail.com>
2023-05-29 14:00:02 +0530

    Green random word min length

--- test_hagman.py
+++ test_hagman.py
@@ -1,7 +1,20 @@
 
 import hangman
-from wordlist import hanglist
+import os
+import tempfile
 
+def test_select_random_word_min_length():
+    # create temporary file
+    name = tempfile.mktemp()
+    f = open(name, "w")
+    f.writelines(["cat\n","elephant\n","mouse\n","dog\n"])
+    f.close()
+
+    for _ in range(20):
+        secret_word = hangman.get_random_word(name)
+        assert secret_word == "elephant"
+
+    os.unlink(name)
 
 def test_masked_word():
     assert hangman.masked_word("elephant",['e'])== 'e_e_____'
