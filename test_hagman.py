1872476a2082afb588dc9fc9fc8b8157a25d0ace
Akshaisali <akshaisali007@gmail.com>
2023-05-29 14:13:55 +0530

    Green random word no capitals

--- test_hagman.py
+++ test_hagman.py
@@ -27,7 +27,18 @@ def test_select_random_word_no_non_alpha_chars():
         assert secret_word == "policeman"
 
     os.unlink(name)
+def test_select_random_word_no_capitals():
+    # create temporary file
+    name = tempfile.mktemp()
+    f = open(name, "w")
+    f.writelines(["Alexander\n","AMD\n","California\n","pelican\n"])
+    f.close()
 
+    for _ in range(20):
+        secret_word = hangman.get_random_word(name)
+        assert secret_word == "pelican"
+
+    os.unlink(name)
 
 def test_masked_word():
     assert hangman.masked_word("elephant",['e'])== 'e_e_____'
