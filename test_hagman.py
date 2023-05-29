d04a805a0a65b93447fbb4d89fbee68a6889cb2a
Akshaisali <akshaisali007@gmail.com>
2023-05-29 14:10:55 +0530

    Green random word no non alpha characters

--- test_hagman.py
+++ test_hagman.py
@@ -15,6 +15,19 @@ def test_select_random_word_min_length():
         assert secret_word == "elephant"
 
     os.unlink(name)
+def test_select_random_word_no_non_alpha_chars():
+    # create temporary file
+    name = tempfile.mktemp()
+    f = open(name, "w")
+    f.writelines(["pine's\n","Dr.\n","Ångström\n","policeman\n"])
+    f.close()
+
+    for _ in range(20):
+        secret_word = hangman.get_random_word(name)
+        assert secret_word == "policeman"
+
+    os.unlink(name)
+
 
 def test_masked_word():
     assert hangman.masked_word("elephant",['e'])== 'e_e_____'
